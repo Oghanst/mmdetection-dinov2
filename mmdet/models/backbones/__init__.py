91bd0f361266041411f2443e6129878a7b4a2637
yang <fan-yang@sjtu.edu.cn>
2023-12-26 17:15:02 +0800

    add focalnet

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -23,5 +23,6 @@ __all__ = [
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
     'SwinTransformer', 'PyramidVisionTransformer',
-    'PyramidVisionTransformerV2', 'EfficientNet', 'CSPNeXt'
+    'PyramidVisionTransformerV2', 'EfficientNet', 'CSPNeXt',
+    'FocalNet',
 ]
